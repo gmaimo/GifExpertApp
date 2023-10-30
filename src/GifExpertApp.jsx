81abd1e067c1c3bdadfdcdd2ab8b231a599d0887
Gabriel Maimó <gabrielmaimorigo@gmail.com>
2023-10-30 20:59:34 +0100

    create categories & add a category

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import {useState} from 'react';
 
 export const GifExpertApp = () => {
-  return (
-    <>
+    
+    const [categories, setCategories] = useState(['Fútbol']);
+
+    const onAddCategory = () => {
+        setCategories([ "Fortnite", ...categories ]);
+        //setCategories( cat => [...categories, "Fortnite"]);
+    }
+
+    return (
+        <>
+        {/* Título */}
         <h1>GifExpertApp</h1>
-    </>
-  )
+        
+        {/* Input */}
+        
+        
+        {/* Listado de gifs */}
+        <button onClick={onAddCategory}>Agregar</button>
+        <ol>
+            { categories.map( category => { return <li key={ category }>{category}</li>})}
+        </ol>
+        {/* Gif Item */}
+        </>
+    )
 }
+    
